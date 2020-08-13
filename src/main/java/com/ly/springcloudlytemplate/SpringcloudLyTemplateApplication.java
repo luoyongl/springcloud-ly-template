080cfeba0584ab5dd011893107c572c403b18c96
luoyong <32861555+luoyongl@users.noreply.github.com>
2020-08-13 21:02:27 +0800

    first commit

---
+++ src/main/java/com/ly/springcloudlytemplate/SpringcloudLyTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ly.springcloudlytemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLyTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudLyTemplateApplication.class, args);
+    }
+
+}
